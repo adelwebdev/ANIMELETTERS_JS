3dc2f0e0493d9252adc9da65d6efff54af83b522
ADEL BOUATTA <83227627+adelwebdev@users.noreply.github.com>
2022-01-18 00:12:33 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const createLetter = () => {
     target.appendChild(letter);
 
     letter.classList.add('letter');
-    // letter.style.opacity = '0';
+    letter.style.opacity = '0';
 
     letter.style.animation = 'anim 5s ease forwards';
     letter.textContent = array[wordIndex][letterIndex];
@@ -18,4 +18,23 @@ const createLetter = () => {
     }, 2000)
 }
 
-createLetter();
+const loop = () => {
+    setTimeout(() => {
+        if (wordIndex >= array.length){
+            wordIndex = 0;
+            letterIndex = 0;
+            loop(); 
+        } else if (letterIndex < array[wordIndex].length){
+            createLetter();
+            letterIndex++;
+            loop();
+        } else {
+            letterIndex = 0;
+            wordIndex++
+            setTimeout(() => {
+                loop();
+            }, 2000)
+        }
+    }, 80)
+}
+loop();
